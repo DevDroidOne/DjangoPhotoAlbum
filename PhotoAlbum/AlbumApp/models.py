39eee36b9907f0d4e36a10656f1f7b8dd2ad990f
DevDroidOne <jancob14@gmail.com>
2021-11-06 15:55:57 +0000

    ManageUsers

--- PhotoAlbum/AlbumApp/models.py
+++ PhotoAlbum/AlbumApp/models.py
@@ -2,12 +2,14 @@ from django.db import models
 from django.contrib.auth.models import User
 # Create your models here.
 class Album(models.Model):
+    user = models.ForeignKey(User,on_delete=models.CASCADE)
     name = models.CharField(max_length=100,null=False,blank= False)
 
     def __str__(self):
         return self.name
 
 class Photo(models.Model):
+    user = models.ManyToManyField(User)
     album = models.ForeignKey(Album, on_delete=models.SET_NULL,null=True,blank=True)
     image = models.ImageField(null=False,blank=False)
     description = models.TextField()
